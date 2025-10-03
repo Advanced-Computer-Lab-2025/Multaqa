21c8653d9a29e43922d6d9e81d1066f0782fa1ef
Chainingrobin <youssefyd04@gmail.com>
2025-10-03 20:20:11 +0300

    progress(Events):Delete Endpoint

--- backend/services/eventService.ts
+++ backend/services/eventService.ts
@@ -1,6 +1,8 @@
+import { forbidden } from "joi";
 import { IEvent } from "../interfaces/ievent.interface";
 import GenericRepository from "../repos/genericRepo";
 import { Event } from "../schemas/eventSchema"; // Adjust the path as needed
+import { ConflictException } from "node-http-exceptions";
 
 export class EventsService {
   private eventRepo: GenericRepository<IEvent>;
@@ -14,5 +16,14 @@ export class EventsService {
 
   async getEventById(id: string): Promise<IEvent | null> {
     return await this.eventRepo.findById(id);
+  }
+
+  async deleteEvent(id: string): Promise<IEvent | null> {
+    const event = await this.eventRepo.findById(id);
+    if (!event || (event.attendees.length > 0)) {
+      //will replace with a throw later
+      return null;
+    }
+    return await this.eventRepo.delete(id);
   }
 }
