cfa1b70bb0171d1cd2e57ee3715257fcd5daa4a0
sultann-301 <91574726+sultann-301@users.noreply.github.com>
2025-10-08 18:58:48 +0300

    chore(events): rename to eventRepo

--- backend/services/eventService.ts
+++ backend/services/eventService.ts
@@ -3,80 +3,82 @@ import GenericRepository from "../repos/genericRepo";
 import { Event } from "../schemas/event-schemas/eventSchema";
 import { User } from "../schemas/stakeholder-schemas/userSchema";
 import createError from "http-errors";
-import "../schemas/event-schemas/workshopEventSchema"; 
+import "../schemas/event-schemas/workshopEventSchema";
 import "../schemas/event-schemas/bazaarEventSchema";
 import "../schemas/event-schemas/platformBoothEventSchema";
-import "../schemas/stakeholder-schemas/staffMemberSchema"
-import "../schemas/stakeholder-schemas/vendorSchema"
+import "../schemas/stakeholder-schemas/staffMemberSchema";
+import "../schemas/stakeholder-schemas/vendorSchema";
 
 export class EventsService {
-  private eventgeneralRepo: GenericRepository<IEvent>;
+  private eventRepo: GenericRepository<IEvent>;
 
   constructor() {
-    this.eventgeneralRepo = new GenericRepository(Event);
-   
+    this.eventRepo = new GenericRepository(Event);
   }
 
+  async getEvents(
+    search?: string,
+    type?: string,
+    location?: string,
+    sort?: boolean
+  ) {
+    const filter: any = {};
+    if (type) filter.type = type;
+    if (location) filter.location = location;
 
- async getEvents(search?: string, type?: string, location?: string, sort?: boolean) {
-  const filter: any = {};
-  if (type) filter.type = type;
-  if (location) filter.location = location;
-
-
-
-
-let events = await this.eventgeneralRepo.findAll(filter, {
-  populate: [
-    { path: "associatedProfs", select: "firstName lastName email" },
-    { path: "vendors", select: "companyName email logo" },
-    { path: "vendor", select: "companyName email logo" },
-  ] as any
-});
-
-  
-  if (sort) {
-    events = events.sort((a: any, b: any) => {
-      return new Date(a.event_start_date).getTime() - new Date(b.event_start_date).getTime();
+    let events = await this.eventRepo.findAll(filter, {
+      populate: [
+        { path: "associatedProfs", select: "firstName lastName email" },
+        { path: "vendors", select: "companyName email logo" },
+        { path: "vendor", select: "companyName email logo" },
+      ] as any,
     });
+
+    if (sort) {
+      events = events.sort((a: any, b: any) => {
+        return (
+          new Date(a.event_start_date).getTime() -
+          new Date(b.event_start_date).getTime()
+        );
+      });
+    }
+
+    if (search) {
+      const searchRegex = new RegExp(search, "i");
+      return events.filter(
+        (event: any) =>
+          searchRegex.test(event.event_name) ||
+          searchRegex.test(event.type) ||
+          event.associatedProfs?.some(
+            (prof: any) =>
+              searchRegex.test(prof?.firstName) ||
+              searchRegex.test(prof?.lastName)
+          )
+      );
+    }
+
+    return events;
   }
-
-  
-  if (search) {
-    const searchRegex = new RegExp(search, "i");
-    return events.filter(
-      (event: any) =>
-        searchRegex.test(event.event_name) || 
-        searchRegex.test(event.type) ||
-        event.associatedProfs?.some((prof: any) => 
-          searchRegex.test(prof?.firstName) || 
-          searchRegex.test(prof?.lastName)
-        )
-    );
-  }
-
-  return events;
-}
-
-
-
-
 
   async getEventById(id: string): Promise<IEvent | null> {
-    const options = { populate: [{ path: "associatedProfs", select: "firstName lastName email" },
-    { path: "vendors", select: "companyName email logo" },
-    { path: "vendor", select: "companyName email logo" },
-    {path: "attendees", select:"firstName lastName email gucId "} as any] };
-    const event = await this.eventgeneralRepo.findById(id, options);
+    const options = {
+      populate: [
+        { path: "associatedProfs", select: "firstName lastName email" },
+        { path: "vendors", select: "companyName email logo" },
+        { path: "vendor", select: "companyName email logo" },
+        { path: "attendees", select: "firstName lastName email gucId " } as any,
+      ],
+    };
+    const event = await this.eventRepo.findById(id, options);
     return event;
   }
 
   async deleteEvent(id: string): Promise<IEvent | null> {
-    const event = await this.eventgeneralRepo.findById(id);
+    const event = await this.eventRepo.findById(id);
     console.log("THE EVENT GETTING DELETEDDD", event);
     if (event && event.attendees && event.attendees.length > 0) {
       throw createError(409, "Cannot delete event with attendees");
     }
-    return await this.eventgeneralRepo.delete(id);
+    return await this.eventRepo.delete(id);
   }
 }
