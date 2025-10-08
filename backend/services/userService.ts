62492e999934a172035d7dd188f305b19d1579f6
sultann-301 <91574726+sultann-301@users.noreply.github.com>
2025-10-08 23:44:25 +0300

    refactor(backend): added select to generic repo
    +refactored users

--- backend/services/userService.ts
+++ backend/services/userService.ts
@@ -1,22 +1,24 @@
 import { IStudent } from "../interfaces/student.interface";
 import { IUser } from "../interfaces/user.interface";
 import GenericRepository from "../repos/genericRepo";
-import { UserRepository } from "../repos/userRepo";
 import { User } from "../schemas/stakeholder-schemas/userSchema";
-
 
 export class UserService {
   private userRepo: GenericRepository<IUser>;
-  private userRepository: UserRepository;
   constructor() {
     this.userRepo = new GenericRepository(User);
-    this.userRepository = new UserRepository();
   }
-   async getAllUsers(): Promise<Partial<IUser>[]> {
-    return this.userRepository.findUsers();
+  async getAllUsers(): Promise<Partial<IUser>[]> {
+    return this.userRepo.findAll(
+      { isVerified: true },
+      {
+        select:
+          "firstName lastName name email role gucId position roleType status companyName",
+      }
+    );
   }
 
-    async getUserById(id: string): Promise<IUser | null> {
+  async getUserById(id: string): Promise<IUser | null> {
     return await this.userRepo.findById(id);
   }
 }
