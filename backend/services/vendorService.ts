6586e5b7b60b64be5abb6c689ea05c0b5f5ba055
Chainingrobin <youssefyd04@gmail.com>
2025-10-10 00:16:21 +0300

    progress(Vendor):type casting

--- backend/services/vendorService.ts
+++ backend/services/vendorService.ts
@@ -1,17 +1,19 @@
 import { IEvent } from "../interfaces/event.interface";
 import { IUser } from "../interfaces/user.interface";
+import { IVendor } from "../interfaces/vendor.interface";
 import GenericRepository from "../repos/genericRepo";
 import { Event } from "../schemas/event-schemas/eventSchema";
 import { User } from "../schemas/stakeholder-schemas/userSchema";
 import createError from "http-errors";
+import { Vendor } from "../schemas/stakeholder-schemas/vendorSchema";
 
 export class VendorService {
-  private userRepo: GenericRepository<IUser>;
+  private vendorRepo: GenericRepository<IVendor>;
   private eventRepo: GenericRepository<IEvent>;
 
   constructor() {
     // Vendor is a discriminator of User, so use User model to query vendors
-    this.userRepo = new GenericRepository(User);
+    this.vendorRepo = new GenericRepository(Vendor);
     this.eventRepo = new GenericRepository(Event);
   }
 
@@ -22,27 +24,16 @@ export class VendorService {
    * - If vendor not found -> throw 404
    * - Map populated requestedEvents to an array of events, filtering out any nulls
    */
-  async getVendorEvents(id: string): Promise<Partial<IEvent>[]> {
+  async getVendorEvents(id: string): Promise<Partial<any>> {
     // populate the nested 'event' field inside requestedEvents
-    const vendor = await this.userRepo.findById(id, {
-      populate: [
-        "requestedEvents.event",
-        // If you want to select specific fields from event, use the dot notation with select in the schema or handle after population
-      ],
+    const vendor = await this.vendorRepo.findById(id, {
+      populate: ["requestedEvents.event"],
     });
 
     if (!vendor) {
       throw createError(404, "Vendor not found");
     }
 
-    // vendor.requestedEvents is an array of { event: Event | ObjectId, status }
-    // Use any casts because IUser doesn't strictly type requestedEvents here
-    const anyVendor: any = vendor;
-
-    const events: Partial<IEvent>[] = (anyVendor.requestedEvents || [])
-      .map((re: any) => re && re.event)
-      .filter(Boolean);
-
-    return events;
+    return (vendor as any).requestedEvents;
   }
 }
