bb5cce05a40828a3ed7239524ddd7ea7fae6ece1
Hatem Soliman <hatemthedev@gmail.com>
2025-10-10 22:22:11 +0300

    merge: merge entity-wrapper into role-selection

--- client/src/components/RegistredComponent/utils/index.tsx
+++ client/src/components/RegistredComponent/utils/index.tsx
@@ -1,20 +1,31 @@
-import {TruncatedTextProps} from "../types"
-import {Tooltip, Typography} from '@mui/material';
+import { TruncatedTextProps } from "../types";
+import { Tooltip, Typography } from "@mui/material";
 
-export const TruncatedText: React.FC<TruncatedTextProps> = ({ children, maxChars = 40 , fontSize, fontWeight="600"}) => {
-  const size:number = children.length;
+export const TruncatedText: React.FC<TruncatedTextProps> = ({
+  children,
+  maxChars = 40,
+  fontSize,
+  fontWeight = "600",
+}) => {
+  const size: number = children.length;
   const textComponent = (
     <Typography
       variant="body2"
+      onPointerDown={(e) => {
+        // This stops the drag event from being triggered when clicking on the text,
+        // allowing mouse hover events for the tooltip to work correctly.
+        e.stopPropagation();
+      }}
       sx={{
-        color: '#1A1A1A',
-        overflow: 'hidden',
-        textOverflow: 'ellipsis',
-        whiteSpace: 'nowrap',
-        maxWidth: '100%',
+        color: "#1A1A1A",
+        overflow: "hidden",
+        textOverflow: "ellipsis",
+        whiteSpace: "nowrap",
+        maxWidth: "100%",
         fontSize: fontSize,
         fontWeight: fontWeight,
-        fontFamily: "var(--font-poppins), system-ui, sans-serif"
+        fontFamily: "var(--font-poppins), system-ui, sans-serif",
+        cursor: "default", // Explicitly set cursor to default for text
       }}
     >
       {children}
@@ -30,4 +41,4 @@ export const TruncatedText: React.FC<TruncatedTextProps> = ({ children, maxChars
 
   // If the text is short, return the Typography component alone
   return textComponent;
-}
+};
