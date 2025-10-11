420d38020e463bd1c55ade1585c9f8b753146eb2
sultann-301 <91574726+sultann-301@users.noreply.github.com>
2025-10-11 15:55:56 +0300

    feature(events): update trips/bazaars

---
+++ backend/validation/validateUpdateEvent.ts
@@ -0,0 +1,39 @@
+import Joi from "joi";
+import { EVENT_TYPES } from "../constants/events.constants";
+
+// Event base schema
+const eventBaseSchema = Joi.object({
+  type: Joi.string()
+    .valid(...Object.values(EVENT_TYPES))
+    .required(),
+  eventName: Joi.string().required(),
+  eventStartDate: Joi.date().required(),
+  eventEndDate: Joi.date().required(),
+  eventStartTime: Joi.string().required(),
+  eventEndTime: Joi.string().required(),
+  registrationDeadline: Joi.date().required(),
+  location: Joi.string().required(),
+  description: Joi.string().required(),
+});
+
+// Bazaar event schema
+const bazaarSchema = eventBaseSchema.keys({
+  price: Joi.forbidden(),
+});
+
+// Trip event schema
+const tripSchema = eventBaseSchema.keys({
+  capacity: Joi.number().min(1).required(),
+  price: Joi.number().min(0).required(),
+});
+
+export function validateUpdateEvent(data: any) {
+  switch (data.type) {
+    case "bazaar":
+      return bazaarSchema.validate(data, { abortEarly: false });
+    case "trip":
+      return tripSchema.validate(data, { abortEarly: false });
+    default:
+      return eventBaseSchema.validate(data, { abortEarly: false });
+  }
+}
