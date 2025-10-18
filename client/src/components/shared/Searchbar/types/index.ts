932ee640bb8b3e50c108d0e456c51d1e98e50a44
Hatem Soliman <hatemthedev@gmail.com>
2025-10-19 00:25:39 +0300

    feat: when you search in browse-events.tsx (lines 178-201), you can search across all those base fields plus the type-specific fields

--- client/src/components/shared/Searchbar/types/index.ts
+++ client/src/components/shared/Searchbar/types/index.ts
@@ -3,6 +3,8 @@
 export interface CustomSearchProps {
   icon?: boolean;
   width?: string;
-  type?:"inwards"|"outwards",
+  type?: "inwards" | "outwards",
   label?: string;
+  value?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
