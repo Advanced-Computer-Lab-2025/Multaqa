4e1f1dba0b4ddc685a148e1a3911e70d66f03831
YasmeenTarek1 <yasmeen.tarek.elabsawy@gmail.com>
2025-10-18 17:15:34 +0300

    fix(Integration): removing duplicate AuthProviders

--- client/src/app/[locale]/login/page.tsx
+++ client/src/app/[locale]/login/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import LoginForm from "@/components/shared/LoginForm/LoginForm";
-import { AuthProvider } from "@/context/AuthContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 export default function LoginPage() {
   return (
-    <AuthProvider>
+    <>
       <div
         className="flex flex-col items-center justify-center"
         style={{ transform: "scale(0.8)" }}
@@ -14,6 +13,6 @@ export default function LoginPage() {
         <LoginForm />
       </div>
       <ToastContainer />
-    </AuthProvider>
+    </>
   );
 }
