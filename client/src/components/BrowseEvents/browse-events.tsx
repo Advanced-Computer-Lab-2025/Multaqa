78f08683e937c01ef998fe16532b2167ccf3299c
Mahmoud Elkabbany <mahmoudelkabbany292@gmail.com>
2025-11-12 08:04:23 +0200

    fix(RegistrationStatus): Add extra status
    Add extra status in browse-events to toggle register button in browse-events tab and my registered events tab

--- client/src/components/BrowseEvents/browse-events.tsx
+++ client/src/components/BrowseEvents/browse-events.tsx
@@ -158,6 +158,8 @@ const BrowseEvents: React.FC<BrowseEventsProps> = ({
     setReady(true);
   };
 
+  const registeredEvents = userInfo.registeredEvents;
+  
   const handleRegistered = () => {
     setLoading(true);
     console.log(userInfo);
@@ -328,6 +330,7 @@ const BrowseEvents: React.FC<BrowseEventsProps> = ({
             agenda={event.agenda}
             user={user}
             registered={registered}
+            isRegisteredEvent={registeredEvents.map((e: any) => e._id).includes(event.id)}
             userInfo={UserInfo}
             onDelete={() => handleDeleteEvent(event.id)}
             isReady={isReady}
@@ -347,6 +350,7 @@ const BrowseEvents: React.FC<BrowseEventsProps> = ({
             description={event.description}
             user={user}
             registered={registered}
+            isRegisteredEvent={registeredEvents.map((e: any) => e._id).includes(event.id)}
             userInfo={UserInfo}
             onDelete={() => handleDeleteEvent(event.id)}
             isReady={isReady}
@@ -366,6 +370,7 @@ const BrowseEvents: React.FC<BrowseEventsProps> = ({
             details={event.details}
             user={user}
             registered={registered}
+            isRegisteredEvent={registeredEvents.map((e: any) => e._id).includes(event.id)}
             userInfo={UserInfo}
             onDelete={() => handleDeleteEvent(event.id)}
             isReady={isReady}
@@ -384,6 +389,7 @@ const BrowseEvents: React.FC<BrowseEventsProps> = ({
             description={event.description}
             user={user}
             registered={registered}
+            isRegisteredEvent={registeredEvents.map((e: any) => e._id).includes(event.id)}
             userInfo={UserInfo}
             onDelete={() => handleDeleteEvent(event.id)}
             isReady={isReady}
