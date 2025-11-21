fa5e43f913b89a46707a0cbe70eaa0592e5f26cf
YasmeenTarek1 <yasmeen.tarek.elabsawy@gmail.com>
2025-11-21 03:57:41 +0200

    progress(notifications): correctly collecting the usersToNotify

--- backend/utils/eventListeners.ts
+++ backend/utils/eventListeners.ts
@@ -24,86 +24,76 @@ function sendSocketNotification(typeNotification: string, notification: INotific
 
   const userService = new UserService();
   if (notification.role) {
-    notification.role.forEach((role: UserRole) => {
-      switch (role) {
-        case UserRole.ADMINISTRATION:
-          if (notification.adminRole) {
-            switch (notification.adminRole) {
-              case AdministrationRoleType.EVENTS_OFFICE:
-                (async () => {
-                  const eventsOfficeAdmins = await userService.getAllEventsOffice();
-                  eventsOfficeAdmins.forEach((admin) => {
-                    if (!usersToNotify.includes(String(admin._id))) {
-                      usersToNotify.push(String(admin._id));
-                    }
-                  });
-                })();
-                break;
-              case AdministrationRoleType.ADMIN:
-                // Fetch all administration
-                (async () => {
-                  const allAdmins = await userService.getAllAdmins();
-                  allAdmins.forEach((admin) => {
-                    if (!usersToNotify.includes(String(admin._id))) {
-                      usersToNotify.push(String(admin._id));
-                    }
-                  });
-                })();
-                break;
-            }
-          }
-          break;
-        case UserRole.STAFF_MEMBER:
-          if (notification.staffPosition) {
-            switch (notification.staffPosition) {
-              case StaffPosition.PROFESSOR:
-                (async () => {
-                  const professors = await userService.getAllProfessors();
-                  professors.forEach((professor) => {
-                    if (!usersToNotify.includes(String(professor._id))) {
-                      usersToNotify.push(String(professor._id));
-                    }
-                  });
-                })();
-                break;
-              case StaffPosition.TA:
-                (async () => {
-                  const tas = await userService.getAllTAs();
-                  tas.forEach((ta) => {
-                    if (!usersToNotify.includes(String(ta._id))) {
-                      usersToNotify.push(String(ta._id));
-                    }
-                  });
-                })();
-                break;
-              case StaffPosition.STAFF:
-                (async () => {
-                  const staffMembers = await userService.getAllStaff();
-                  staffMembers.forEach((staff) => {
-                    if (!usersToNotify.includes(String(staff._id))) {
-                      usersToNotify.push(String(staff._id));
-                    }
-                  });
-                })();
-                break;
-            }
-          }
-          break;
-        case UserRole.STUDENT:
-          // Fetch all students
+    if (notification.role.includes(UserRole.ADMINISTRATION)) {
+      if (notification.adminRole) {
+        if (notification.adminRole.includes(AdministrationRoleType.EVENTS_OFFICE)) {
           (async () => {
-            const students = await userService.getAllStudents();
-            students.forEach((student) => {
-              if (!usersToNotify.includes(String(student._id))) {
-                usersToNotify.push(String(student._id));
+            const eventsOfficeAdmins = await userService.getAllEventsOffice();
+            eventsOfficeAdmins.forEach((admin) => {
+              if (!usersToNotify.includes(String(admin._id))) {
+                usersToNotify.push(String(admin._id));
               }
             });
           })();
-          break;
+        }
+        if (notification.adminRole.includes(AdministrationRoleType.ADMIN)) {
+          (async () => {
+            const allAdmins = await userService.getAllAdmins();
+            allAdmins.forEach((admin) => {
+              if (!usersToNotify.includes(String(admin._id))) {
+                usersToNotify.push(String(admin._id));
+              }
+            });
+          })();
+        }
       }
-    });
+    }
+    if (notification.role.includes(UserRole.STAFF_MEMBER)) {
+      if (notification.staffPosition) {
+        if (notification.staffPosition.includes(StaffPosition.PROFESSOR)) {
+          (async () => {
+            const professors = await userService.getAllProfessors();
+            professors.forEach((professor) => {
+              if (!usersToNotify.includes(String(professor._id))) {
+                usersToNotify.push(String(professor._id));
+              }
+            });
+          })();
+        }
+        if (notification.staffPosition.includes(StaffPosition.TA)) {
+          (async () => {
+            const tas = await userService.getAllTAs();
+            tas.forEach((ta) => {
+              if (!usersToNotify.includes(String(ta._id))) {
+                usersToNotify.push(String(ta._id));
+              }
+            });
+          })();
+        }
+        if (notification.staffPosition.includes(StaffPosition.STAFF)) {
+          (async () => {
+            const staffMembers = await userService.getAllStaff();
+            staffMembers.forEach((staff) => {
+              if (!usersToNotify.includes(String(staff._id))) {
+                usersToNotify.push(String(staff._id));
+              }
+            });
+          })();
+        }
+      }
+    }
+    if (notification.role.includes(UserRole.STUDENT)) {
+      (async () => {
+        const students = await userService.getAllStudents();
+        students.forEach((student) => {
+          if (!usersToNotify.includes(String(student._id))) {
+            usersToNotify.push(String(student._id));
+          }
+        });
+      })();
+    }
   }
-  
+
   usersToNotify.forEach((userId) => {
     const sockets = OnlineUsersService.getUserSockets(userId);
     sockets.forEach((socketId) => io.to(socketId).emit(typeNotification, notification));
