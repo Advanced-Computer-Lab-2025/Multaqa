ef9c0f2fd7e26b80a46906fbc99c770e22536d7b
EsraaTarek382 <esraatareksamir@gmail.com>
2025-12-01 08:25:43 +0200

    fix(conference): adding validations that prevent editing a conference date to a past one

--- client/src/components/tempPages/EditConference/EditConference.tsx
+++ client/src/components/tempPages/EditConference/EditConference.tsx
@@ -25,8 +25,8 @@ import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 const validationSchema = yup.object({
     eventName: yup.string().required('Conference Name is required').min(3, 'Name must be at least 3 characters'),
     description: yup.string().required('Description is required'),
-    eventStartDate: yup.string().required('Start Date is required'),
-    eventEndDate: yup.string().required('End Date is required'),
+    eventStartDate: yup.date().required('Start date is required').min(new Date(), "Start date can't be in the past"),
+    eventEndDate: yup.date().required('End date is required').min(yup.ref('eventStartDate'), "End date must be after start date"),
     requiredBudget: yup.number().typeError('Budget must be a number').positive('Budget must be positive').required('Budget is required'),
     fundingSource: yup.string().required('Funding Source is required'),
     websiteLink: yup.string().required('Link is required'),
