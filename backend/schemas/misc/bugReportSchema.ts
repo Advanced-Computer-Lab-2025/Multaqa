14822adccdec2878e9c8afa4e468706959c9fece
EsraaTarek382 <esraatareksamir@gmail.com>
2025-12-08 09:00:47 +0200

    feature(bugReport)Add bug report model, schema, and constants
    Introduces BUG_REPORT_STATUS enum, IBugReport interface, and Mongoose schema for bug reports. This provides a structured way to store and manage bug reports in the backend.

---
+++ backend/schemas/misc/bugReportSchema.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from 'mongoose';
+import { IBugReport } from '../../interfaces/models/bugReport.interface';
+import { BUG_REPORT_STATUS } from '../../constants/bugReport.constants';
+
+const bugReportSchema = new Schema<IBugReport>({
+    title: { type: String, required: true },
+    stepsToReproduce: { type: String, required: true },
+    expectedBehavior: { type: String, required: true },
+    actualBehavior: { type: String, required: true },
+    enviroment: { type: String, required: true },
+    reporterId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    status: { type: String, enum: Object.values(BUG_REPORT_STATUS), default: BUG_REPORT_STATUS.PENDING},
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+});
+
+bugReportSchema.set("toObject", { virtuals: true });
+bugReportSchema.set("toJSON", { virtuals: true });
+
+export const BugReport = mongoose.model<IBugReport>('BugReport', bugReportSchema);
+
