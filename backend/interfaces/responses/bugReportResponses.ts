705a95b813a3320cdf4a06db38a8e2f4ebb66528
EsraaTarek382 <esraatareksamir@gmail.com>
2025-12-09 07:12:34 +0200

    Refactor bug report interfaces and schema
    Renamed 'reporterId' to 'reporter' in IBugReport interface and bugReportSchema for consistency. Extended IBugReport to inherit from Document. Added response interfaces for bug report API responses.

---
+++ backend/interfaces/responses/bugReportResponses.ts
@@ -0,0 +1,14 @@
+import { IBugReport } from "../models/bugReport.interface";
+
+
+export  interface IBugReportResponse {
+    success: boolean;
+    data:IBugReport | IBugReport[];
+    message: string;
+}
+
+export  interface IUpdateBugReportStatusResponse {
+    success: boolean;
+    message: string;
+}
+   
